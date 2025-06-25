028c7cc43012e4980aab5f9a0735277e1c4f0eb1
mohdomi <omairm.s.chouhan@gmail.com>
2025-06-25 12:01:50 +0000

    fix2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import FaceMatch from "./pages/FaceMatch";
